da3093965f27649c373fd5abbef44b3a1a2b49be
getChan <9511chn@gmail.com>
2020-01-08 15:50:50 +0900

    publish server;

--- server/app.py
+++ server/app.py
@@ -1,17 +1,18 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 import demo_run
-app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def demo():
-    if request.method == "POST":
-        query = request.form.get('query')
-        # model predict
-        print(query)
-        result = demo_run.translate(query)
-        return render_template("demo.html", result=result, query=query)
-    else:
-        return render_template('demo.html')
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0') 
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/', methods=['GET', 'POST'])
+    def demo():
+        if request.method == "POST":
+            query = request.form.get('query')
+            # model predict
+            print(query)
+            result = demo_run.translate(query)
+            return render_template("demo.html", result=result, query=query)
+        else:
+            return render_template('demo.html')
+        
+    return app
